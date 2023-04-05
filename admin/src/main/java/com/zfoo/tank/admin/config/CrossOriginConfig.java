36512a49f1f6b0c139ffa961157a1897397b7f94
godotg <jaysunxiao.gmail.com>
2023-04-05 14:20:34 +0800

    feat[tank]: support vue3 and vuetify3

--- admin/src/main/java/com/zfoo/tank/admin/config/CrossOriginConfig.java
+++ admin/src/main/java/com/zfoo/tank/admin/config/CrossOriginConfig.java
@@ -12,15 +12,9 @@
 
 package com.zfoo.tank.admin.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.List;
 
 /**
  * @author godotg
