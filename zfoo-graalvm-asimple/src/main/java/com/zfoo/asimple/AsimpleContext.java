163a206f5947c4da0fdd5a58e49c8a4ad9df7469
godotg <jaysunxiao@gmail.com>
2023-09-02 00:04:57 +0800

    test[graalvm]: test graalvm

---
+++ zfoo-graalvm-asimple/src/main/java/com/zfoo/asimple/AsimpleContext.java
@@ -0,0 +1,13 @@
+package com.zfoo.asimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsimpleContext {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsimpleContext.class, args);
+    }
+
+}
