7549a70dd85b94226b97e7236e72b014dae785b4
godotg <jaysunxiao@gmail.com>
2023-09-03 21:18:24 +0800

    test[graalvm]: graalvm protocol test

---
+++ zfoo-graalvm-protocol/src/main/java/com/zfoo/graalvm/protocol/packet/ObjectB.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2020 The zfoo Authors
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+
+package com.zfoo.graalvm.protocol.packet;
+
+
+import com.zfoo.protocol.anno.Protocol;
+
+import java.util.Objects;
+
+/**
+ * @author godotg
+ * @version 3.0
+ */
+@Protocol(id = 103)
+public class ObjectB {
+
+    private boolean flag;
+
+    public boolean isFlag() {
+        return flag;
+    }
+
+    public void setFlag(boolean flag) {
+        this.flag = flag;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ObjectB objectB = (ObjectB) o;
+        return flag == objectB.flag;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(flag);
+    }
+}
+
