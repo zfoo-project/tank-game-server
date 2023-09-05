4a41ac760f525b75f3f2192b11ac3895ab0be32b
sun <jaysunxiao@gmail.com>
2023-09-05 15:38:19 +0800

    feat[net]: graalvm support

---
+++ zfoo-graalvm-net/src/main/java/com/zfoo/graalvm/net/ApplicationContext.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2020 The zfoo Authors
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+
+package com.zfoo.graalvm.net;
+
+import com.zfoo.net.core.HostAndPort;
+import com.zfoo.net.core.tcp.TcpServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+
+@SpringBootApplication
+public class ApplicationContext {
+
+    private static final Logger logger = LoggerFactory.getLogger(ApplicationContext.class);
+
+    public static void main(String[] args) throws IOException {
+        SpringApplication.run(ApplicationContext.class, args);
+
+        var server = new TcpServer(HostAndPort.valueOf("127.0.0.1:9000"));
+        server.start();
+    }
+
+}
