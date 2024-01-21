f282e19e7a2ad6bd715fb1962dae8ce67509cae5
godotg <jaysunxiao@gmail.com>
2024-01-21 23:31:24 +0800

    update

---
+++ gateway/src/test/java/com/zfoo/tank/gateway/client/MyTankClientController.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2020 The zfoo Authors
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+
+package com.zfoo.tank.gateway.client;
+
+import com.zfoo.net.anno.PacketReceiver;
+import com.zfoo.net.session.Session;
+import com.zfoo.protocol.util.JsonUtils;
+import com.zfoo.tank.common.protocol.CurrencyUpdateNotice;
+import com.zfoo.tank.common.protocol.PlayerExpNotice;
+import com.zfoo.tank.common.protocol.battle.BattleResultResponse;
+import com.zfoo.tank.common.protocol.cache.ScoreRankResponse;
+import com.zfoo.tank.common.protocol.login.GetPlayerInfoResponse;
+import com.zfoo.tank.common.protocol.login.LoginByHttpTokenResponse;
+import com.zfoo.tank.common.protocol.login.LoginResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author jaysunxiao
+ */
+@Component
+public class MyTankClientController {
+
+    private static final Logger logger = LoggerFactory.getLogger(MyTankClientController.class);
+
+
+    @PacketReceiver
+    public void aLoginByHttpTokenResponse(Session session, LoginByHttpTokenResponse response) {
+        logger.info("客户端收到服务器的登录返回[{}]", JsonUtils.object2String(response));
+    }
+
+
+    @PacketReceiver
+    public void atGetPlayerInfoResponse(Session session, GetPlayerInfoResponse response) {
+        var playerInfo = response.getPlayerInfo();
+        var currencyVo = response.getCurrencyVo();
+
+        logger.info("玩家基础信息[id:{}][name:{}]", playerInfo.getId(), playerInfo.getName());
+
+        MyTankData.playerInfo = playerInfo;
+        MyTankData.currencyVo = currencyVo;
+    }
+
+
+    @PacketReceiver
+    public void atBattleResultResponse(Session session, BattleResultResponse response) {
+        logger.info("战斗结果[{}]", JsonUtils.object2String(response));
+    }
+
+    @PacketReceiver
+    public void atScoreRankResponse(Session session, ScoreRankResponse response) {
+        logger.info("排行榜信息[{}]", JsonUtils.object2String(response));
+    }
+
+    @PacketReceiver
+    public void atCurrencyUpdateNotice(Session session, CurrencyUpdateNotice notice) {
+        logger.info("currency update [{}]", JsonUtils.object2String(notice));
+    }
+
+    @PacketReceiver
+    public void atPlayerExpNotice(Session session, PlayerExpNotice response) {
+        logger.info("player exp update [{}]", JsonUtils.object2String(response));
+    }
+
+
+}
---
+++ gateway/src/test/java/com/zfoo/tank/gateway/client/MyTankClientTest.java
@@ -0,0 +1,150 @@
+/*
+ * Copyright (C) 2020 The zfoo Authors
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+
+package com.zfoo.tank.gateway.client;
+
+import com.zfoo.net.NetContext;
+import com.zfoo.net.core.HostAndPort;
+import com.zfoo.net.core.tcp.TcpClient;
+import com.zfoo.net.util.NetUtils;
+import com.zfoo.protocol.util.JsonUtils;
+import com.zfoo.protocol.util.RandomUtils;
+import com.zfoo.protocol.util.ThreadUtils;
+import com.zfoo.scheduler.util.TimeUtils;
+import com.zfoo.tank.common.constant.TankDeployEnum;
+import com.zfoo.tank.common.protocol.battle.BattleResultRequest;
+import com.zfoo.tank.common.protocol.cache.ScoreRankRequest;
+import com.zfoo.tank.common.protocol.cache.ScoreRankResponse;
+import com.zfoo.tank.common.protocol.login.LoginByHttpTokenRequest;
+import com.zfoo.tank.common.protocol.login.LoginRequest;
+
+import static com.zfoo.tank.common.util.HttpLoginUtils.*;
+
+import com.zfoo.tank.gateway.Application;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.function.Consumer;
+
+/**
+ * @author jaysunxiao
+ */
+@Ignore
+public class MyTankClientTest {
+
+    private static final Logger log = LoggerFactory.getLogger(MyTankClientTest.class);
+
+    static {
+        TankDeployEnum.initDefaultEnv();
+    }
+
+    @Test
+    public void tankClient() throws Exception {
+        var context = new ClassPathXmlApplicationContext("my-tank-application.xml");
+
+        // http登录
+        var loginResult = loginByHttp("玩家3");
+
+        var myTankClient = new TcpClient(HostAndPort.valueOf(NetUtils.LOCAL_LOOPBACK_IP, Application.TCP_SERVER_PORT));
+        var myTankSession = myTankClient.start();
+
+        // 模拟客户端，发送一个登录请求
+        var loginRequest = LoginByHttpTokenRequest.valueOf(loginResult.getMyToken());
+        NetContext.getRouter().send(myTankSession, loginRequest);
+        ThreadUtils.sleep(2 * TimeUtils.MILLIS_PER_SECOND);
+
+        // 发送一个战斗结果请求
+        var battleResultRequest = BattleResultRequest.valueOf(RandomUtils.randomInt());
+        NetContext.getRouter().send(myTankSession, battleResultRequest);
+        ThreadUtils.sleep(TimeUtils.MILLIS_PER_SECOND);
+
+        // 发送一个获取分数排行榜的信息
+        var scoreRankRequest = ScoreRankRequest.valueOf();
+        NetContext.getRouter().send(myTankSession, scoreRankRequest);
+        ThreadUtils.sleep(TimeUtils.MILLIS_PER_SECOND);
+
+        var scoreRankSyncResponse = NetContext.getRouter().syncAsk(myTankSession, scoreRankRequest, ScoreRankResponse.class, null).packet();
+        log.info("sync 排行榜 [{}]", JsonUtils.object2String(scoreRankSyncResponse));
+        ThreadUtils.sleep(TimeUtils.MILLIS_PER_SECOND);
+
+        NetContext.getRouter().asyncAsk(myTankSession, scoreRankRequest, ScoreRankResponse.class, null)
+                .whenComplete(new Consumer<ScoreRankResponse>() {
+                    @Override
+                    public void accept(ScoreRankResponse scoreRankResponse) {
+                        log.info("async 排行榜 [{}]", JsonUtils.object2String(scoreRankResponse));
+                    }
+                });
+        ThreadUtils.sleep(TimeUtils.MILLIS_PER_SECOND);
+    }
+
+
+    // ---------------------------------------------------------------------------------------------------------
+
+    public HttpLoginResult loginByHttp(String name) throws IOException, InterruptedException {
+        var client = HttpClient.newBuilder().build();
+
+        var responseBodyHandler = HttpResponse.BodyHandlers.ofString();
+
+        var loginRequest = new HttpLoginRequest();
+        loginRequest.setToken(name);
+
+        var request = HttpRequest
+                .newBuilder(URI.create("http://localhost:13001/api/login"))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(JsonUtils.object2String(loginRequest)))
+                .build();
+
+        var json = client.send(request, responseBodyHandler).body();
+        log.info("http login [{}]", json);
+
+        var response = JsonUtils.string2Object(json, HttpLoginResponse.class);
+        return response.getData();
+    }
+
+    public static class HttpLoginResponse {
+        private int code;
+        private String message;
+        private HttpLoginResult data;
+
+        public int getCode() {
+            return code;
+        }
+
+        public void setCode(int code) {
+            this.code = code;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+
+        public HttpLoginResult getData() {
+            return data;
+        }
+
+        public void setData(HttpLoginResult data) {
+            this.data = data;
+        }
+    }
+}
