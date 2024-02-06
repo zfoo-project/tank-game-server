96bcce2ed5711b7eddf90a7cbd70928fb20d322e
godotg <jaysunxiao@gmail.com>
2024-02-06 14:36:26 +0800

    feat[push]: new push module

---
+++ push/src/main/java/com/zfoo/tank/cache/config/AutoConfiguration.java
@@ -0,0 +1,20 @@
+
+
+package com.zfoo.tank.cache.config;
+
+import com.zfoo.net.config.model.NetConfig;
+import com.zfoo.orm.config.OrmConfig;
+import com.zfoo.storage.config.StorageConfig;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class AutoConfiguration {
+    @Bean
+    @ConfigurationProperties(prefix = "net")
+    public NetConfig netConfig() {
+        return new NetConfig();
+    }
+}
---
+++ push/src/main/java/com/zfoo/tank/cache/controller/PushController.java
@@ -0,0 +1,10 @@
+package com.zfoo.tank.cache.controller;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author godotg
+ */
+@Component
+public class PushController {
+}
